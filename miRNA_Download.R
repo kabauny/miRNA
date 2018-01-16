496927220e857b623bd0200d4a3da1c23dea3337
Jim Chen <kabauny@gmail.com>
2018-01-15 19:59:59 -0600

    Update miRNA_Download.R

--- miRNA_Download.R
+++ miRNA_Download.R
@@ -34,7 +34,6 @@ LUSC_name = miRNA_data_LUSC$miRNA_ID
 miRNA_data_LUSC$miRNA_ID <- NULL
 LUSC = miRNA_data_LUSC[c(TRUE, FALSE, FALSE)]
 LUSC$name = LUSC_name
-LUSC$label = rep('LUSC', nrow(LUSC))
 
 write.csv(LUSC, 'miRNA_LUSC.csv')
 
@@ -51,6 +50,5 @@ LUAD_name = miRNA_data_LUAD$miRNA_ID
 miRNA_data_LUAD$miRNA_ID <- NULL
 LUAD = miRNA_data_LUAD[c(TRUE, FALSE, FALSE)]
 LUAD$name = LUAD_name
-LUAD$label = rep('LUAD', nrow(LUAD))
 
 write.csv(LUAD, 'miRNA_LUAD.csv')
