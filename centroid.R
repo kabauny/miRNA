40203d7da881d2cdccb573ecd95bf77fa96f07c8
Jim Chen <kabauny@gmail.com>
2018-01-15 23:31:26 -0600

    Delete centroid.R

--- centroid.R
+++
@@ -1,6 +0,0 @@
-library(pamr)
-predictors = read.csv('/Users/zhongningchen/LungCancer/Data')
-label = read.csv('/Users/zhongningchen/LungCancer/Label')
-predictors = predictors[,2:ncol(predictors)]
-label = label[,2:ncol(label)]
-miRNA.data = list(x = t(predictors), y = label, genenames = colnames(predictors))
