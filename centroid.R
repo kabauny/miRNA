ee7fa5d4521d782719d3661e12b6e073e064e557
Jim Chen <kabauny@gmail.com>
2018-02-07 15:40:35 -0600

    Update centroid.R

--- centroid.R
+++ centroid.R
@@ -1,42 +1,26 @@
-library(pamr)
-parent = "/Users/zhongningchen/Data"
-tumorStageList = c("stage_ia", "stage_ib", "stage_iia", "stage_iib", "stage_iiia", "stage_iiib", "stage_iv")
-###############################################
+
 #core components
 ReadmiRNA = function(cancerType, sample, tumorStage){
   #creates dataframe given Cancer type and tumor stage. 
   #p x n
   #p name := miRNA name
   #n name := cancer_stage_index
-  if (sample != "Normal"){
-    f = paste(parent, cancerType, sample, tumorStage, sep = "/")
-  } else{
-    f = paste(parent, cancerType, sample, sep = "/")
-  }
-  
+
+  f = paste(parent, cancerType, sample, tumorStage, sep = "/")
+
   manif = paste(f, "MANIFEST.txt", sep = "/")
   manifest = read.table(file = manif, sep = "\t", header = TRUE)
   fileName = paste(f, toString(manifest$filename[1]), sep = "/")
   dft = read.table(file = fileName, sep = "\t", header = TRUE)
   df = data.frame(dft$read_count)
-  
-  if (sample != "Normal"){
-    colnames(df)[1] = paste(cancerType, tumorStage, "1", sep = "_")
-  } else{
-    colnames(df)[1] = paste(cancerType, "Normal", "1", sep = "_")
-  }
-  
+  colnames(df)[1] = paste(cancerType, tumorStage, "1", sep = "_")
+
   rownames(df) = dft$miRNA_ID
   
   for(i in 2:length(manifest$filename)){
     fileName = paste(f, toString(manifest$filename[i]), sep = "/")
     dft = read.table(file = fileName, sep = "\t", header = TRUE)
-    if (sample != "Normal"){
-      tempSampleName = paste(cancerType, tumorStage, toString(i), sep = "_")
-    } else{
-      tempSampleName = paste(cancerType, "Normal", toString(i), sep = "_")
-    }
-    
+    tempSampleName = paste(cancerType, tumorStage, toString(i), sep = "_")
     df[tempSampleName] = dft$read_count
   }
   
@@ -81,54 +65,42 @@ join = function(a,b){
 }
 ##############################################
 #cgts = centroid given cancer tyoe and tumor stage
-cgts = function(ts){
-  adeno = ReadmiRNA(cancerType = "LUAD", sample = "PrimaryTumor", tumorStage = ts)
-  adeno = add_Label(adeno, "LUAD")
-  sq = ReadmiRNA(cancerType = "LUSC", sample = "PrimaryTumor", tumorStage = ts)
-  sq = add_Label(sq, "LUSC")
-  
-  adsq = join(a = adeno, b = sq)
-  X = adsq[,names(adsq) != 'label']
-  miRNA.data = list(x = t(X), y = adsq$label, genenames = colnames(X), geneid = colnames(X))
-  return(miRNA.data)
-}
-
-normcgts = function(ct, ts){
-  p = ReadmiRNA(cancerType = ct, sample = "PrimaryTumor", tumorStage = ts)
-  p = add_Label(p, ct)
-  #q = ReadmiRNA(cancerType = ct, sample = "Normal")
-  q = ReadNormal()
-  q = add_Label(q, "Normal")
-  
-  pq = join(a = p, b = q)
-  X = pq[,names(pq) != 'label']
-  miRNA.data = list(x = t(X), y = pq$label, genenames = colnames(X), geneid = colnames(X))
-  return(miRNA.data)
-}
 
 diffgeneList = function(ct, ts){
-  miRNA = normcgts(ct, ts)
-  miRNA.train = pamr.train(miRNA)
-  deltaIndex = which.min(miRNA.train$errors)
-  delta = miRNA.train$threshold[deltaIndex]
-  lg = pamr.listgenes(miRNA.train, miRNA, threshold=delta)
-  lg = data.frame(lg)
-  return(lg)
-}
+  q = normal
+  q = add_Label(q, "Normal")
+  
+  df = ReadmiRNA(cancerType = ct, sample = "PrimaryTumor", tumorStage = ts)
+  p = add_Label(df, ct)
 
-adj = function(ct, ts){
-  b = diffgeneList(ct, ts)
-  df = ReadmiRNA(ct, "PrimaryTumor", ts)
-  return(df[row.names(df) %in% b$id,])   
+  pq = rbind(a = p, b = q)
+  X = pq[,names(pq) != 'label']
+  
+  miRNA.data = list(x = t(X), y = pq$label, genenames = colnames(X), geneid = colnames(X))
+  
+  miRNA.train = pamr.train(miRNA.data)
+  miRNA.cv = pamr.cv(miRNA.train, miRNA.data)
+  deltaIndex = which.min(miRNA.cv$error)
+  delta = miRNA.cv$threshold[deltaIndex]
+  miRNA.confusion = pamr.confusion(miRNA.cv, delta)
+  
+  lg = pamr.listgenes(miRNA.train, miRNA.data, threshold=delta)
+  lg = data.frame(lg)
+
+  dl = df[row.names(df) %in% lg$id,]
+  
+  return(list(dl, df))  
 }
 
 acgts = function(ts){
   #adjusted centroid given cancer type and tumor stage
-  adeno = ReadmiRNA(cancerType = "LUAD", sample = "PrimaryTumor", tumorStage = ts)
-  squamous = ReadmiRNA(cancerType = "LUSC", sample = "PrimaryTumor", tumorStage = ts)
+  temp = diffgeneList("LUAD", ts)
+  ad = data.frame(temp[1])
+  adeno = data.frame(temp[2])
   
-  ad = adj("LUAD", ts)
-  sq = adj("LUSC", ts)
+  temp = diffgeneList("LUSC", ts)
+  sq = data.frame(temp[1])
+  squamous = data.frame(temp[2])
   
   ad2 = adeno[row.names(adeno) %in% row.names(sq),]
   sq2 = squamous[row.names(squamous) %in% row.names(ad),]
@@ -139,7 +111,6 @@ acgts = function(ts){
   ad3 = rbind(ad2, atemp)
   sq3 = rbind(sq2, stemp)
   
-  
   ad4 = add_Label(ad3, "LUAD")
   sq4 = add_Label(sq3, "LUSC")
 
@@ -148,10 +119,28 @@ acgts = function(ts){
   X = adsq[,names(adsq) != 'label']
   miRNA.data = list(x = t(X), y = adsq$label, genenames = colnames(X), geneid = colnames(X))
   
-  print(length(ad3))
-  print(length(sq3))
-  return(miRNA.data)
+  miRNA.train = pamr.train(miRNA.data)
+  miRNA.cv = pamr.cv(miRNA.train, miRNA.data)
+  deltaIndex = which.min(miRNA.cv$error)
+  delta = miRNA.cv$threshold[deltaIndex]
+  miRNA.confusion = pamr.confusion(miRNA.cv, delta)
+  
+  lg = pamr.listgenes(miRNA.train, miRNA.data, threshold=delta)
+  lg = data.frame(lg)
+  
+  write.csv(lg$id, paste("/Users/zhongningchen/Data/miRNA/shrukenCentroid", ts, sep = "/"), quote = FALSE, row.names = FALSE)
+  return(lg$id)
+
 }
 
-ac = acgts(tumorStageList[6])
-pamr.menu(ac)
+library(pamr)
+parent = "/Users/zhongningchen/Data/miRNA"
+tumorStageList = c("stage_ia", "stage_ib", "stage_iia", "stage_iib", "stage_iii", "stage_iv")
+normal = ReadNormal()
+
+ts = tumorStageList[5]
+#tempVariable = acgts(ts)
+
+ 
+
+
